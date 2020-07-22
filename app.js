4b1c8dfd70eff045ce7c171b9dace3dfbd920964
Ilona Pak <inkha.pak@gmail.com>
2020-07-22 17:45:44 +0700

    change mongoose db string to env

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const eventRouter = require("./routes/events");
-const authRouter = require("./routes/auth")
-const newsRouter = require("./routes/news")
-const mediaRouter = require("./routes/media")
+const authRouter = require("./routes/auth");
+const newsRouter = require("./routes/news");
+const mediaRouter = require("./routes/media");
 
-require("dotenv").config()
+require("dotenv").config();
 
 var app = express();
-
 mongoose
-  .connect("mongodb://localhost/arcan")
+  .connect(process.env.DB)
   .then(() => console.log("connected to database"));
 
 app.use(logger("dev"));
@@ -25,13 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors())
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 app.use("/events", eventRouter);
-app.use("/news", newsRouter)
-app.use("/media", mediaRouter)
+app.use("/news", newsRouter);
+app.use("/media", mediaRouter);
 
 module.exports = app;
