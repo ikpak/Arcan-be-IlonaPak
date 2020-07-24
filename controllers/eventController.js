c8940fdda166fceee9d3f7f202f6278072c03ba7
Ilona Pak <inkha.pak@gmail.com>
2020-07-24 13:39:47 +0700

    fix

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -13,8 +13,6 @@ exports.getUpcomingEvents = async (req, res) => {
   const events = await Event.find({
     date: { $gt: new Date() },
   });
-  
-  const sortedByDate = events.sort((a, b) => a.date - b.date);
 
   res.send(sortedByDate);
   // "OrderDate" : { "$gt" : { "$date" : "2015-01-01"}}
